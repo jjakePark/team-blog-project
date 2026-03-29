b3a787f5f6f7cdb5aaf4cdccc2b52264bf88fd20
rohga19 <rohga19@gmail.com>
2026-03-29 21:24:03 +0900

    진짜프론트환경설정끝

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,7 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Layout from './assets/pages/Layout'
+import Home from './assets/pages/Home';
+
 function App() {
 
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
